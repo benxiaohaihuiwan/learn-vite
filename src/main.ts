4578d4ecc467cc89cc153c91d5072dac63e23c68
happyCoder <744209932@qq.com>
2024-05-09 20:27:07 +0800

    add： 增加ant-design-vue插件的引入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "unocss"; // 引入 unocss 原子化
+import "ant-design-vue/dist/reset.css"; // 引入ant-design
+import Antd from "ant-design-vue"; // 先全局引入ant-design-vue
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+async function bootstrap() {
+  const app = createApp(App);
+  // 引用 ant-design-vue
+  app.use(Antd);
+
+  app.mount("#app");
+}
+
+bootstrap();
