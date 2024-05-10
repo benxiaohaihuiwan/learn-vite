4f1d8e04cad3b7c362bcfb7a04050524b704e77a
happyCoder <744209932@qq.com>
2024-05-10 17:22:57 +0800

    add: 增加@别名

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Inspector from 'vite-plugin-vue-inspector';
 
+import  {resolve}  from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     port: 5175
+  },
+  resolve:{
+    alias:{
+      "@": resolve(__dirname,'./src')
+    }
   },
   plugins: [vue(),Inspector({
     openInEditorHost: 'http://localhost:5173',
